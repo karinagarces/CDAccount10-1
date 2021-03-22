84bfcbb675e11a7299dffb0feeb67e8dfba487d1
karinagarces <77650293+karinagarces@users.noreply.github.com>
2021-03-22 19:13:23 -0400

    Add files via upload

---
+++ CD Account Test/CD Account Test/CDAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test 
+{
+    class CDAccount : SavingsAccount
+    {
+        private string _maturityDate;
+
+        public CDAccount()
+        {
+            _maturityDate = "";
+        }
+
+        public string MaturityDate
+        {
+            get { return _maturityDate;  }
+            set { _maturityDate = value; }
+        }
+    }
+}
---
+++ CD Account Test/CD Account Test/SavingsAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class SavingsAccount
+    {
+        // Fields
+        private string _accountNumber;
+        private decimal _interestRate;
+        private decimal _balance;
+
+        public SavingsAccount()
+        {
+            _accountNumber = "";
+            _interestRate = 0;
+            _balance = 0;
+        }
+
+        public string AccountNumber
+        {
+            get { return _accountNumber;  }
+            set { _accountNumber = value;  }
+        }
+
+        public decimal InterestRate
+        {
+            get { return _interestRate; }
+            set { _interestRate = value; }
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+    }
+}
